e78adf0a7fac3c91913dd7fdf13de35ef05425a9
Royal <royal@royaltheartist.com>
2025-10-03 19:23:05 -0600

    May add this back in

--- screen.base.ts
+++ screen.base.ts
@@ -3,7 +3,6 @@ import { Engine } from "./engine";
 import { IRenderable } from "./render/surface";
 
 export abstract class BaseScreen implements IUpdate, IInitialize, IRenderable {
-    engine: Engine | undefined
     ready: boolean = false
     async preload(): Promise<this> {
         this.ready = true
